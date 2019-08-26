09680353535b7d5c6087319ae2e5868cc3bc265a
Shane Gooch <shanegooch@Shanes-iMac.local>
2019-08-26 09:30:33 -0700

    cleaned up code on auth route

--- auth/auth-route.js
+++ auth/auth-route.js
@@ -35,12 +35,10 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
-  console.log(username);
-  console.log(password);
+
   Users.find({ username })
     .first()
     .then(user => {
-      console.log(user);
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = getJwt(user);
         res.status(200).json({
