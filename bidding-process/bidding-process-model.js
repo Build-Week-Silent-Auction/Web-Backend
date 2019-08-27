32c915982d6f944de7bec0715b89888f3abc128f
Shane Gooch <shanegooch@Shanes-iMac.local>
2019-08-27 14:55:19 -0700

    addecd bit route and model

--- bidding-process/bidding-process-model.js
+++ bidding-process/bidding-process-model.js
@@ -1,11 +1,31 @@
 const db = require("../database/db-config.js");
 
 module.exports = {
+  find,
   add
 };
 
-function add(auctionsid, bid) {
-  return db("auctions")
-    .where("auctions.id", auctionsid)
-    .insert({ bid: bid });
+function find(userid) {
+  return db("bids")
+    .innerJoin("users", "users.id", "bids.user_id")
+    .innerJoin("auctions", "auctions.id", "bids.auction_id")
+    .select(
+      "bids.id",
+      "users.username",
+      "auctions.auction_name",
+      "auctions.start_time",
+      "auctions.end_time",
+      "bids.bid"
+    )
+    .where("bids.user_id", userid);
+}
+
+function add(userid, auctionsid, bid) {
+  const addedBid = {
+    ...bid,
+    user_id: userid,
+    auction_id: auctionsid
+  };
+  console.log(addedBid);
+  return db("bids").insert(addedBid);
 }
--- bidding-process/bidding-process-route.js
+++ bidding-process/bidding-process-route.js
@@ -3,20 +3,37 @@ const router = require("express").Router();
 const Bids = require("./bidding-process-model.js");
 
 //GET user bids
-router.get("/:userid/bids", (req, res) => {});
-
-//GET single bid
-router.get("/:userid/bids/:bidid", (req, res) => {
-  res.status(200);
+router.get("/:userid/bids", (req, res) => {
+  const { userid } = req.params;
+  console.log(userid);
+  Bids.find(userid)
+    .then(bids => {
+      res.status(200).json(bids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error fetching bids from database." });
+    });
 });
+
+// //GET single bid
+// router.get("/:userid/bids/:bidid", (req, res) => {
+//   const { biddid } = req.params;
+//   Bids.findBy(biddid)
+//     .then(bids => {
+//       res.status(200).json(bids);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "Error fetching bids from database." });
+//     });
+// });
 
 //Place new bid
 router.post("/:userid/bids/:auctionid", (req, res) => {
-  const { auctionid } = req.params;
+  const { userid, auctionid } = req.params;
+  console.log(req.params);
   const bid = req.body;
-  console.log(bidderid);
 
-  Bids.add(auctionid, bid)
+  Bids.add(userid, auctionid, bid)
     .then(added => {
       res.status(201).json({ message: "Your bid has been placed." });
     })
